f3845ad25963994ffe0b84d8dfca8bcc26b9b03d
Jonathan Braga <jonathan.braga@onclick.local>
2018-08-01 17:34:19 -0300

    Ajustando metas das rotas

--- src/components/theme/header/Header.vue
+++ src/components/theme/header/Header.vue
@@ -1,14 +1,15 @@
 <template>
     <header>
         <v-toolbar app flat color="white">
-            <!-- <img :src="image" alt="200x45" width="200" height="45" class="toolbar__logo"> -->
             <div class="toolbar__logo"> {{ image }} </div>
             <v-spacer></v-spacer>
             <UserAvatar class="toolbar__avatar" :avatarSize="'52px'" />
         </v-toolbar>
+
         <v-spacer></v-spacer>
+
         <v-toolbar class="toolbar__page" flat style="">
-            <v-toolbar-title> Nome da Rota </v-toolbar-title>
+            <v-toolbar-title> {{ list }} </v-toolbar-title>
         </v-toolbar>
     </header>
 </template>
@@ -23,6 +24,12 @@
                 type: String,
                 default: ''
             }
+        },
+        created () {
+            console.log('ROUTER ==> ', this.$router.matched)
+        },
+        computed: {
+            list: x => 'a'
         },
         data () {
             return {
