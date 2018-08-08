6c64b8454d288e1261ea21217985adb2429c98a3
Jonathan Braga <jou_braga@hotmail.com>
2018-08-08 14:17:37 -0300

    Alert ok

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import '@babel/polyfill'
+import '@/assets/css/style.scss'
 import Vue from 'vue'
 import './plugins/vuetify'
 import { ENDPOINT } from './api/config'
 import App from './App.vue'
 import router from './router/index'
 import store from './store'
-import '@/assets/css/style.scss'
 import VueResource from 'vue-resource'
 import VueSweetalert2 from 'vue-sweetalert2'
 
